3d9a27b42fc064543cb2b779396843418d2087c7
César Núñez <75745356+cesarnunezh@users.noreply.github.com>
2025-03-01 17:16:49 -0600

    Join data done

--- andes_indus/__main__.py
+++ andes_indus/__main__.py
@@ -4,7 +4,7 @@ from sodapy import Socrata
 from crime_utils import get_crime_data, Crime
 from merge_shp import load_pumas_shp, load_neighborhood_shp, gen_chi_bbox, gen_quadtree, load_schools, School
 from census_utils import process_multiple_years
-from join_data import grouped_data_by
+from join_data import gen_final_data
 import pandas as pd
 from pathlib import Path
 from education import get_all_school_ids, fetch_school_profiles, save_to_csv
@@ -14,75 +14,60 @@ import webbrowser
 
 def main():
 
-    # Creating all the API and APP Keys for the data sources. 
-    try:
-        CHICAGO_APP_TOKEN = os.environ["CHICAGO_APP_TOKEN"]
-    except KeyError:
-        raise Exception(
-            "Make sure that you have set the APP Token environment variable as described in the README."
-        )
-
-    # Gathering the crime data from the City of Chicago Data web
-    client = Socrata("data.cityofchicago.org", CHICAGO_APP_TOKEN, timeout=10)
-    crime_code = "ijzp-q8t2"
-    homicides_code = "gumc-mgzr"
-    lst_years = list(range(2021, 2024))
-
-    # Creating the pd.Dataframes for crime and homicides_data
-    crime_data = get_crime_data(client, crime_code, lst_years)
-    homicides_data = get_crime_data(client, homicides_code, lst_years)
-
-    # INSERT EDUCATION CODE
-    # school_ids = get_all_school_ids()  # Fetch School IDs
-    # if school_ids:
-    #     school_profiles_df = fetch_school_profiles(school_ids)  # Fetch selected school details
-    #     save_to_csv(school_profiles_df)
-
-    path_schools = Path('data/cps_school_profiles.csv')
-    schools_data = load_schools(path_schools)
-
-    # INSERT CENSUS CODE
-    # process_multiple_years("data/census_df.csv")
-    census_data = pd.read_csv("data/census_df.csv")
-
-    # Merging crime and school data to pumas
-    path_pumas = Path("data/shapefiles/pumas/pumas2022")
-    path_neighborhoods = Path("data/shapefiles/chicomm/chicomm")
-    pumas = load_pumas_shp(path_pumas)
-    neighborhoods = load_neighborhood_shp(path_neighborhoods)
-
-    quadtree_chi_pumas = gen_quadtree(pumas, gen_chi_bbox(pumas))
-    quadtree_chi_neighborhoods = gen_quadtree(neighborhoods, gen_chi_bbox(neighborhoods))
-
-    crimes_by_puma = grouped_data_by(crime_data, quadtree_chi_pumas, 'puma')
-    schools_by_puma = grouped_data_by(schools_data, quadtree_chi_pumas, 'puma')
-
-    crimes_by_neighborhood = grouped_data_by(crime_data, quadtree_chi_neighborhoods, 'neighborhood')
-    schools_by_neighborhood = grouped_data_by(schools_data, quadtree_chi_neighborhoods, 'neighborhood')
+    gen_final_data()
     
-    pumas_shp = gpd.read_file('data/shapefiles/pumas/pumas2022.shp')
-    z = fm.Map(location = [41.8783874319104, -87.62875352665596], tiles='cartodbpositron', zoom_start = 10.5)
+    # pumas_shp = gpd.read_file('data/shapefiles/pumas/pumas2022.shp')
+    # neighborhoods_shp = gpd.read_file('data/shapefiles/chicomm/chicomm.shp')
+    # z = fm.Map(location = [41.8783874319104, -87.62875352665596], tiles='cartodbpositron', zoom_start = 10.5)
+    
+    # fm.Choropleth(
+    #     geo_data=neighborhoods_shp,
+    #     data=schools_by_neighborhood,
+    #     columns=['neighborhood', 'total_students'],
+    #     key_on="feature.properties.CHICOMNO",
+    #     fill_color="YlOrRd",
+    #     fill_opacity=0.8,
+    #     line_opacity=0.2,
+    #     legend_name="Attendance rate (%)",
+    #     smooth_factor=0,
+    #     Highlight= True,
+    #     line_color = "#0000",
+    #     overlay=True,
+    #     nan_fill_color = "White"  # fill white missing values 
+    #     ).add_to(z)
 
-    fm.Choropleth(
-        geo_data=pumas_shp,
-        data=census_data,
-        columns=['PUMA', 'attendance_rate_high'],
-        key_on="feature.properties.PUMACE20",
-        fill_color="YlOrRd",
-        fill_opacity=0.8,
-        line_opacity=0.2,
-        legend_name="Attendance rate (%)",
-        smooth_factor=0,
-        Highlight= True,
-        line_color = "#0000",
-        overlay=True,
-        nan_fill_color = "White"  # fill white missing values 
-        ).add_to(z)
+    # highlight_function = lambda x: {'fillColor': '#000000', 
+    #                             'color':'#000000', 
+    #                             'fillOpacity': 0.5, 
+    #                             'weight': 0.1}
 
-    z.save("visualizations/high_school_rate.html")
+    # breakpoint()
+    # # schools_by_puma = schools_by_puma.rename(columns={"puma": "PUMACE20"})
+    # schools_by_neighborhood = schools_by_neighborhood.rename(columns={"neighborhood": "CHICOMNO"})
+
+    # # pumas_shp["PUMACE20"] = pd.to_numeric(pumas_shp["PUMACE20"])
+
+    # # data_both = pd.merge(pumas_shp, schools_by_puma, how="inner", on="PUMACE20")
+    # data_both = pd.merge(neighborhoods_shp, schools_by_neighborhood, how="inner", on="CHICOMNO")
+    # data_both = data_both.to_json()
+
+    # details = fm.features.GeoJson(
+    #     data = data_both,
+    #     control=False,
+    #     highlight_function=highlight_function, 
+    #     tooltip=fm.features.GeoJsonTooltip(
+    #         fields=['CHICOMNO'],   #Variable selection
+    #         aliases=['Neighborhood'],  # renames
+    #         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+    #     )
+    # )
+
+    # z.add_child(details)
+    # z.keep_in_front(details)
+    # z.save("visualizations/high_school_rate.html")
     
 
 if __name__ == "__main__":
     main()
-    html = Path("visualizations/high_school_rate.html")
-    webbrowser.open_new_tab(html)
+    # html = Path("visualizations/high_school_rate.html")
+    # webbrowser.open_new_tab(html)
--- andes_indus/join_data.py
+++ andes_indus/join_data.py
@@ -1,7 +1,14 @@
 from crime_utils import Crime
-from merge_shp import assign_division, School
+from merge_shp import load_pumas_shp, load_neighborhood_shp, gen_chi_bbox, gen_quadtree, load_schools, assign_division, School
 import pandas as pd
 from quadtree import Quadtree
+import os
+from sodapy import Socrata
+from crime_utils import get_crime_data, Crime
+from pathlib import Path
+from education import get_all_school_ids, fetch_school_profiles, save_to_csv
+from census_utils import process_multiple_years
+import geopandas as gpd
 
 def assign_puma_to_list(data_lst: list[Crime|School], quadtree_chi: Quadtree) -> list[Crime|School]:
     new_data_lst = []
@@ -46,6 +53,7 @@ def grouped_data_by(data_lst: list[Crime|School], quadtree_chi: Quadtree, group:
         final_data = data.groupby([group, 'year', 'primary_type']).size().reset_index(name='Count')
         final_data = final_data.pivot_table(index=[group, 'year'], columns='primary_type', values='Count', fill_value=0)
         final_data = final_data.reset_index()
+        final_data["total_crimes"] = final_data.drop(columns=[group, "year"]).sum(axis=1)
     else:
         numeric_cols = ['student_count', 'graduation_rate']
         boolean_cols = ['is_high_school', 'is_middle_school', 'is_ele_school', 'is_pre_school']
@@ -64,5 +72,87 @@ def grouped_data_by(data_lst: list[Crime|School], quadtree_chi: Quadtree, group:
             if x['graduation_rate'].notna().any() else None
             ).reset_index(name='weighted_hs_grad_rate')
         final_data = final_data.merge(grad_rate, on=group, how='left')
+        final_data['year'] = 2023
     return final_data
+
+def lower_colnames(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Helper function to lower all columns of a dataframe before merging
+    '''
+    data.columns = data.columns.str.lower()
+    return data
+    
+def gen_final_data():
+    # Creating the APP Key for the data sources. 
+    try:
+        CHICAGO_APP_TOKEN = os.environ["CHICAGO_APP_TOKEN"]
+    except KeyError:
+        raise Exception(
+            "Make sure that you have set the APP Token environment variable as described in the README."
+        )
+
+    # Gathering the crime data from the City of Chicago Data web
+    client = Socrata("data.cityofchicago.org", CHICAGO_APP_TOKEN, timeout=10)
+    crime_code = "ijzp-q8t2"
+    homicides_code = "gumc-mgzr"
+    lst_years = list(range(2021, 2024))
+
+    # Creating the pd.Dataframes for crime and homicides_data
+    crime_data = get_crime_data(client, crime_code, lst_years)
+    homicides_data = get_crime_data(client, homicides_code, lst_years)
+
+    # Gathering education data
+    path_schools = Path('data/cps_school_profiles.csv')
+    if path_schools.exists():
+        schools_data = load_schools(path_schools)
+    else:
+        school_ids = get_all_school_ids()  # Fetch School IDs
+        if school_ids:
+            school_profiles_df = fetch_school_profiles(school_ids)  # Fetch selected school details
+            save_to_csv(school_profiles_df)
+        schools_data = load_schools(path_schools)
+
+    # Gathering census data
+    path_census = Path('data/census_df.csv')
+    if path_census.exists():
+        census_data = lower_colnames(pd.read_csv(path_census))
+        census_data["puma"] = census_data["puma"].astype(dtype=str).str.zfill(5)
+    else:
+        process_multiple_years(path_census)
+        census_data = lower_colnames(pd.read_csv(path_census))
+    
+    # Merging crime and school data to pumas
+    path_pumas = Path("data/shapefiles/pumas/pumas2022")
+    pumas = load_pumas_shp(path_pumas)
+    quadtree_chi_pumas = gen_quadtree(pumas, gen_chi_bbox(pumas))
+    crimes_by_puma = lower_colnames(grouped_data_by(crime_data, quadtree_chi_pumas, 'puma'))
+    schools_by_puma = lower_colnames(grouped_data_by(schools_data, quadtree_chi_pumas, 'puma'))
+
+    pumas_shp = gpd.read_file('data/shapefiles/pumas/pumas2022.shp')
+    pumas_shp = pumas_shp.rename(columns={"PUMACE20": "puma"})
+    pumas_shp["puma"] = pumas_shp["puma"].astype(dtype=str).str.zfill(5)
+    
+    pumas_shp = lower_colnames(pumas_shp)
+    
+    data_pumas = pd.merge(crimes_by_puma, schools_by_puma, how="inner", on=["puma", 'year'])
+    data_pumas = pd.merge(data_pumas, census_data, how="inner", on=["puma", 'year'])
+    data_pumas = pd.merge(data_pumas, pumas_shp, how="inner", on=["puma"])
+    print(data_pumas.head())
+    data_pumas.to_csv("data/data_pumas.csv")
+
+    # Merging crime and school data to neighborhoods
+    path_neighborhoods = Path("data/shapefiles/chicomm/chicomm")
+    neighborhoods = load_neighborhood_shp(path_neighborhoods)
+    quadtree_chi_neighborhoods = gen_quadtree(neighborhoods, gen_chi_bbox(neighborhoods))
+    crimes_by_neighborhood = lower_colnames(grouped_data_by(crime_data, quadtree_chi_neighborhoods, 'neighborhood'))
+    schools_by_neighborhood = lower_colnames(grouped_data_by(schools_data, quadtree_chi_neighborhoods, 'neighborhood'))
+    
+    neighborhoods_shp = gpd.read_file('data/shapefiles/chicomm/chicomm.shp')
+    neighborhoods_shp = neighborhoods_shp.rename(columns={"CHICOMNO": "neighborhood"})
+
+    data_neighborhoods = pd.merge(crimes_by_neighborhood, schools_by_neighborhood, how="inner", on=["neighborhood", 'year'])
+    data_neighborhoods = pd.merge(data_neighborhoods, neighborhoods_shp, how="inner", on=["neighborhood"])
+    print(data_neighborhoods.head())
+
+    data_neighborhoods.to_csv("data/data_neighborhoods.csv")
     
